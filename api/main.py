0b45e189b2bcd812a86dfed3a217c7caf36446cd
Milan Lazic <lazic1701@gmail.com>
2025-05-30 15:40:06 +0200

    Remove japanese

--- api/main.py
+++ api/main.py
@@ -27,7 +27,6 @@ GREETINGS: dict[str, str] = {
     Language.FRENCH: "Bonjour le Monde",
     Language.GERMAN: "Hallo Welt",
     Language.DUTCH: "Hallo Wereld",
-    Language.JAPANESE: "こんにちは世界"
 }
 
 app = FastAPI(
